1e88fbd93d1d6be76bef3690c6a847244b12804a
Prakhar pratap singh <imprakhar.ps@gmail.com>
2023-07-23 22:20:03 +0530

    whitenoise

--- foodiewoodie/settings.py
+++ foodiewoodie/settings.py
@@ -141,3 +141,4 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
