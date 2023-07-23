dd76dc6102df3f319b3b81723aeccc1dcec2b1e9
Prakhar pratap singh <imprakhar.ps@gmail.com>
2023-07-23 21:57:20 +0530

    renaming

--- foodiewoodie/settings.py
+++ foodiewoodie/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATICFILES_DIRS=[
-    os.path.join(BASE_DIR, 'foodiewoodie/static/')
+    os.path.join(BASE_DIR, 'static')
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
