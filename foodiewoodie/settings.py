723b518a07f07dcc9f5c99c8297d243e1511bd1a
Prakhar pratap singh <imprakhar.ps@gmail.com>
2023-07-24 08:01:28 +0530

    static-media

--- foodiewoodie/settings.py
+++ foodiewoodie/settings.py
@@ -128,11 +128,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATICFILES_DIRS=[
-    os.path.join(BASE_DIR, 'static_images')
+    os.path.join(BASE_DIR, 'static')
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
