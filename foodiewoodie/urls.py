a861da630edfe8b56ab64bd0ca9cd1a71df219ef
Prakhar pratap singh <imprakhar.ps@gmail.com>
2023-07-24 19:50:14 +0530

    media files.

--- foodiewoodie/urls.py
+++ foodiewoodie/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 import food.views
