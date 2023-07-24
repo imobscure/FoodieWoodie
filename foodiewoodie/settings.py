c0273d17cb3e17adc18b4e4e8e7b5c2b07280026
Prakhar pratap singh <imprakhar.ps@gmail.com>
2023-07-24 07:46:17 +0530

    static-media files

--- foodiewoodie/settings.py
+++ foodiewoodie/settings.py
@@ -132,7 +132,7 @@ STATICFILES_DIRS=[
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
